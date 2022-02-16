e1bc3d9c474567756c5ae9ac98ed2ba26a4531ae
LenzMix <LenzRP@yandex.ru>
2022-02-16 22:00:56 +0500

    2.0.0
    Not shitfull code!

---
+++ SDJailMode/Config.cs
@@ -0,0 +1,99 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SDJailMode
+{
+    public class Config : IDefaultable, IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+            this.multiple = 4;
+            this.minPlayers = 3;
+            this.timerForStart = 10;
+            this.timerForPreGame = 10;
+            this.timerForGame = 600;
+            this.timerForEnd = 10;
+            this.CTspawn = new List<Vector3>();
+            this.Tspawn = new List<Vector3>();
+            this.VoiceMicrophone = new List<Vector3>();
+            this.Doors = new List<Vector3>();
+            this.Lobby = new Vector3();
+            this.MicrophoneRadius = 5f;
+            this.MoneyTWin_T = 10;
+            this.MoneyTWin_CT = 10;
+            this.MoneyTWin_D = 10;
+            this.MoneyCTWin_T = 10;
+            this.MoneyCTWin_CT = 10;
+            this.MoneyCTWin_D = 10;
+            this.MoneyNoTime_T = 10;
+            this.MoneyNoTime_CT = 10;
+            this.MoneyNoTime_D = 10;
+            this.MoneyCTkillT = 10;
+            this.MoneyTkillCT = 10;
+            this.Kits = new List<Kit>();
+            this.StandartKitCT = "";
+            this.StandartKitT = "";
+            this.BlockedPlayersCT = new List<ulong>();
+            this.ItemPoints = new List<ItemPoint>();
+            this.UIID = 23111;
+            this.UIKey = 23111;
+            this.VKLink = "https://";
+            this.DiscordLink = "https://";
+            this.ShopLink = "https://";
+            this.WelcomeText = "Я не пидор\nЯ НАТУРАЛ";
+        }
+
+        public int multiple;
+        public int minPlayers;
+        public int timerForStart;
+        public int timerForPreGame;
+        public int timerForGame;
+        public int timerForEnd;
+        public string StandartKitCT;
+        public string StandartKitT;
+        public string DiscordLink;
+        public string VKLink;
+        public string ShopLink;
+        public string WelcomeText;
+        public decimal MoneyTWin_T;
+        public decimal MoneyTWin_CT;
+        public decimal MoneyTWin_D;
+        public decimal MoneyCTWin_T;
+        public decimal MoneyCTWin_CT;
+        public decimal MoneyCTWin_D;
+        public decimal MoneyNoTime_T;
+        public decimal MoneyNoTime_CT;
+        public decimal MoneyNoTime_D;
+        public decimal MoneyCTkillT;
+        public decimal MoneyTkillCT;
+        public float MicrophoneRadius;
+        public List<Vector3> CTspawn;
+        public List<Vector3> Tspawn;
+        public List<Vector3> Doors;
+        public List<Vector3> VoiceMicrophone;
+        public Vector3 Lobby;
+        public List<Kit> Kits;
+        public List<ulong> BlockedPlayersCT;
+        public List<ItemPoint> ItemPoints;
+        public ushort UIID;
+        public short UIKey;
+
+    }
+
+    public class Kit
+    {
+        public string name;
+        public bool isGet;
+        public bool isCT;
+        public decimal Cost;
+        public string permission;
+        public List<ushort> items;
+    }
+
+    public class ItemPoint
+    {
+        public ushort id;
+        public Vector3 point;
+    }
+}
---
+++ SDJailMode/ModuleDoor.cs
@@ -0,0 +1,112 @@
+﻿using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Framework.Utilities;
+using SDG.NetTransport;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SDJailMode
+{
+    public class ModuleDoor
+    {
+        public void onGesture(UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+        {
+            if (player.GetComponent<PlayerComponent>().team != Team.Police) return;
+            try
+            {
+                Transform raycast = GetBarricadeTransform(player.Player, out BarricadeData data, out BarricadeDrop drop);
+                if (raycast == null) return;
+                if (!Plugin.Instance.Configuration.Instance.Doors.Exists(x => x == raycast.position)) return;
+                if (gesture == UnturnedPlayerEvents.PlayerGesture.PunchLeft && raycast != null)
+                    if (((InteractableDoor)drop.interactable) != null || ((InteractableDoorHinge)drop.interactable) != null) ExecuteOpen(player);
+                if (gesture == UnturnedPlayerEvents.PlayerGesture.PunchRight && raycast != null)
+                    if (((InteractableDoor)drop.interactable) != null || ((InteractableDoorHinge)drop.interactable) != null) ExecuteOpen(player);
+            }
+            catch { }
+        }
+
+        private void ExecuteOpen(UnturnedPlayer player)
+        {
+            Transform raycast = GetBarricadeTransform(player.Player, out BarricadeData data, out BarricadeDrop drop);
+            if (raycast == null) return;
+            if (!Plugin.Instance.Configuration.Instance.Doors.Exists(x => x == raycast.position)) return;
+            var raycastPos = raycast.position;
+            OpenDoor(player);
+        }
+
+        public static void OpenDoor(UnturnedPlayer player)
+        {
+            Transform raycast = GetBarricadeTransform(player.Player, out BarricadeData data, out BarricadeDrop drop);
+            bool OpenNeed = !((InteractableDoor)drop.interactable).isOpen;
+            BarricadeManager.ServerSetDoorOpen((InteractableDoor)drop.interactable, OpenNeed);
+            ClientStaticMethod<bool>
+            SendDoor = ClientStaticMethod<bool>.Get(new ClientStaticMethod<bool>.ReceiveDelegate(((InteractableDoor)drop.interactable).ReceiveOpen));
+            SendDoor.Invoke(ENetReliability.Reliable, Provider.EnumerateClients(), OpenNeed);
+        }
+
+        public static void ChangeDoors(bool isOpen)
+        {
+            if (Plugin.Instance.Configuration.Instance.Doors.Count > 0) foreach (Vector3 points in Plugin.Instance.Configuration.Instance.Doors)
+                {
+                    List<Transform> result = new List<Transform>();
+                    BarricadeManager.getBarricadesInRadius(points, 0.1f, result);
+                    if (result.Count == 0) continue;
+                    Transform raycast = GetBarricadeTransform(result[0], out BarricadeData data, out BarricadeDrop drop);
+                    BarricadeManager.ServerSetDoorOpen((InteractableDoor)drop.interactable, isOpen);
+                    ClientStaticMethod<bool>
+                    SendDoor = ClientStaticMethod<bool>.Get(new ClientStaticMethod<bool>.ReceiveDelegate(((InteractableDoor)drop.interactable).ReceiveOpen));
+                    SendDoor.Invoke(ENetReliability.Reliable, Provider.EnumerateClients(), isOpen);
+                }
+        }
+
+        public static Transform GetBarricadeTransform(Player player, out BarricadeData barricadeData, out BarricadeDrop drop)
+        {
+            barricadeData = null;
+            drop = null;
+            RaycastHit hit;
+            if (PhysicsUtility.raycast(new Ray(player.look.aim.position, player.look.aim.forward), out hit, 25, RayMasks.BARRICADE_INTERACT))
+            {
+                Transform transform = hit.transform;
+                InteractableDoorHinge doorHinge = hit.transform.GetComponent<InteractableDoorHinge>();
+                if (doorHinge != null)
+                {
+                    transform = doorHinge.door.transform;
+                }
+
+                drop = BarricadeManager.FindBarricadeByRootTransform(transform);
+                if (BarricadeManager.tryGetRegion(transform, out _, out _, out _, out BarricadeRegion region))
+                {
+                    barricadeData = drop.GetServersideData();
+                    return drop.model;
+                }
+            }
+
+            return null;
+        }
+
+        public static Transform GetBarricadeTransform(Transform transform, out BarricadeData barricadeData, out BarricadeDrop drop)
+        {
+            barricadeData = null;
+            drop = null;
+            InteractableDoorHinge doorHinge = transform.GetComponent<InteractableDoorHinge>();
+            if (doorHinge != null)
+            {
+                transform = doorHinge.door.transform;
+            }
+
+            drop = BarricadeManager.FindBarricadeByRootTransform(transform);
+            if (BarricadeManager.tryGetRegion(transform, out _, out _, out _, out BarricadeRegion region))
+            {
+                barricadeData = drop.GetServersideData();
+                return drop.model;
+            }
+
+            return transform;
+        }
+    }
+}
